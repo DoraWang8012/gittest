6f169344a8b06d88fbe0785adc33fa9471933a91
Dora Wang <dida_vai1212@yahoo.com.tw>
2021-05-16 15:40:20 +0800

    no man

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld{
 	public static void main (String[]args){
 	System.out.println("Hello World");
-	System.out.println("這是一個平行世界一，這是個有人類的世界");
+	System.out.println("這是一個平行世界二，這是個沒有人類的世界");
 	
 	}
 }
