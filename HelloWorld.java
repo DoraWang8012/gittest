55261c912e41211a8985e5c538adb48f07822280
Dora Wang <dida_vai1212@yahoo.com.tw>
2021-05-16 15:10:28 +0800

    new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 
 public class HelloWorld{
 	public static void main (String[]args){
-	System.out.println("Hello World 世界你不好嗎");
+	System.out.println("Hello World , a damn good world");
+	System.out.println("Hello but you are not my fir);
 	}
 }
